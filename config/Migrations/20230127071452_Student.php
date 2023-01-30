950ae345a35e93a8464ec44b4b04a9870ae847c7
ChanHeinKyaw <scm.chanheinkyaw@gmail.com>
2023-01-30 11:37:47 +0630

    students crud

---
+++ config/Migrations/20230127071452_Student.php
@@ -0,0 +1,23 @@
+<?php
+declare (strict_types = 1);
+
+use Migrations\AbstractMigration;
+
+class Student extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $studentTable = $this->table('students');
+        $studentTable->addColumn('name', 'string');
+        $studentTable->addColumn('email', 'string');
+        $studentTable->addColumn('phone', 'string');
+        $studentTable->create();
+    }
+}
---
+++ templates/Students/view.php
@@ -0,0 +1,25 @@
+<div class="row">
+    <div class="column-responsive column-100">
+        <div class="students view content">
+            <h3><?=$student->name?></h3>
+            <table>
+                <tr>
+                    <th><?=__('Name')?></th>
+                    <td><?=$student->name?></td>
+                </tr>
+                <tr>
+                    <th><?=__('Email')?></th>
+                    <td><?=$student->email?></td>
+                </tr>
+                <tr>
+                    <th><?=__('Id')?></th>
+                    <td><?=$student->id?></td>
+                </tr>
+                <tr>
+                    <th><?=__('Phone')?></th>
+                    <td><?=$student->phone?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
